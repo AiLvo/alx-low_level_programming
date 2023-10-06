0fecc27606833e4b602b7af13baf1a5f17144cf3
AiLvo <elvismurithi42@gmail.com>
2023-10-06 08:08:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char lowercase, uppercase;
+
+    for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+    {
+        putchar(lowercase);
+    }
+
+    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+    {
+        putchar(uppercase);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
