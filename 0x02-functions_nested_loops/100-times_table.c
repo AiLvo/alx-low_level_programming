31879fe7f8e5873072d49d6a6fbaf792f18a5848
AiLvo <elvismurithi42@gmail.com>
2023-10-12 01:37:48 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,32 +9,35 @@
 void print_times_table(int n)
 
 {
-	int x, y, z;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (x = 0; x <= n; x++)
+	for (num = 0; num <= n; num++)
 	{
-	_putchar("0");
-	for (y = 1; y <= n; y++)
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
 	{
-	_putchar("0");
-	_putchar("0");
-	z = num * multi;
-	if (z <= 99)
-	_putchar(" ");
-	if (z <= 9)
-	_putchar(" ");
-	if (z >= 100)
+	_putchar('0');
+	_putchar(',');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 0)
+	_putchar(' ');
+	if (prod >= 100)
 	{
-	_putchar((z / 100) + '0');
-	_putchar(((z / 10)) % 10 + '0');
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
-	else if (z <= 99 && z >= 10)
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
 	}
-	_putchar((z / 10) + '0');
+	_putchar((prod % 10) + '0');
 	}
-	_putchar((z % 10) + '0');
+	_putchar('\n');
+	}
 	}
 }
 
