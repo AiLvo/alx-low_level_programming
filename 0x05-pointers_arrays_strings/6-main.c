fde037618d7766eafa88608a9de90682849fa291
AiLvo <elvismurithi42@gmail.com>
2023-10-16 19:32:11 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code for prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
